699534720c7c5a4888435f77dda6af76fbc1551a
Guilherme <guilhermezgabrielle@gmail.com>
2025-09-22 20:35:55 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Servicos from './pages/Servicos';
